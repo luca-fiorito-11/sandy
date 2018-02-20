95300564d4420744b86fca4a70d9c334b721a5d3
Luca Fiorito <lucafiorito.11@gmail.com>
2018-02-20 23:46:55 +0100

    finished function extract_xs

--- sandy/endf6/files.py
+++ sandy/endf6/files.py
@@ -24,70 +24,7 @@ def split(file):
     return list(filter(None, U)) # remove empty lines
 
 
-
-class Chunk:
-    """
-    MFMT section
-    """
-
-    @property
-    def n(self):
-        r"""
-        Number of lines of the `ENDF-6` file
-        """
-        return len(self.text)
-
-    @property
-    def line(self):
-        """
-        Return current line.
-        """
-        return self.text[self.i]
-
-    def __init__(self, text, mat=None, mf=None, mt=None):
-        from io import StringIO
-        import pandas as pd
-        self.mat = int(text[66:70])
-        self.mf = int(text[70:72])
-        self.mt = int(text[72:75])
-        # first argument must be any object with a read() method (such as a StringIO)
-        self.text = pd.read_fwf(StringIO(text), widths=6*[11]+[4,2,3,5],
-                                names=("c1","c2","l1","l2","n1","n2","mat","mf","mt","ns"),
-                                header=None)
-        self.i = 0
-
-    def __iter__(self):
-        """
-        Make the file an iterator.
-        """
-        return self
-
-    def next(self):
-        """
-        Yield a new line of the `ENDF-6` file.
-        """
-        line = self.line
-        self.i += 1
-        return line
-
-    def move(self, steps):
-        """
-        Move backward/forward a number of lines in the ``ENDF-6`` text.
-
-        Inputs:
-        - :``steps``: :
-            (scalar integer) number of lines to jump
-        """
-        self.i += int(steps)
-
-    def read_cont(self):
-        """
-        Read ``ENDF-6`` ``CONT`` record in formatted fortran.
-
-        Outputs:
-            - :``out``: :
-                (tuple) content of ``CONT`` record
-
+"""
         Found error in:
             - n-17-Cl-035.jeff32
             - n-3-Li-007.jeff32
@@ -101,18 +38,7 @@ class Chunk:
             - n-94-Pu-242.jeff32
             - n-97-Bk-250.jeff32
             - n-98-Cf-254.jeff32
-        """
-        try:
-            out = (float(self.line.c1), float(self.line.c2), float(self.line.l1),
-                   float(self.line.l2), float(self.line.n1), float(self.line.n2))
-            self.i += 1
-            return out
-        except:
-            sys.exit("ERROR: line is not CONT.\n{}".format(self.line))
-
-
-def list2dict(chunks):
-    return 1
+"""
 
 def process_section(text):
     mf = int(text[70:72])
@@ -134,7 +60,8 @@ def read_mf3_mt(text):
     out.update({"ZA" : C.C1, "AWR" : C.C2})
     T, i = read_tab1(str_list, i)
     out.update({"QM" : T.C1, "QI" : T.C2, "LR" : T.L2, "NBR" : T.NBT, "INT" : T.INT})
-    out.update({"XS" : pd.Series(T.y, index = T.x)})
+    out.update({"XS" : pd.Series(T.y, index = T.x, 
+                                 name = (out["MAT"],out["MT"]))})
     return out
 
 
@@ -226,10 +153,13 @@ def pandas_interpolate(df, interp_column, method='zero', axis='both'):
 
 
 def extract_xs(tape):
-    xsdf = pd.DataFrame()
-    for chunk in tape.query('MF==3').DATA:
-        xsdf.add(chunk["XS"],)
-        pass
+    # No interpolation is done because xs are on unionized grid
+    mat=125
+    xsdf = pd.DataFrame(tape.loc[mat,3,1].DATA["XS"])
+    for chunk in tape.query('MF==3 & MT!=1').DATA:
+        xsdf = xsdf.add(pd.DataFrame(chunk["XS"]), fill_value=0)
+    xsdf.fillna(0, inplace=True)
+    return xsdf
 
 def extract_cov33(tape, mt=[102]):
     from sandy.cov import triu_matrix
@@ -281,6 +211,7 @@ def merge_covs(covdf):
     query_diag = 'MT==MT1 & (MAT1==0 | MAT==MAT1)'
     query_offdiag = 'MT!=MT1 | MAT1!=0'
     diags = covdf.query(query_diag)
+    # reset indices to be consistent with enumerate in the next loop
     diags.reset_index(inplace=True)
     # Process diagonal blocks
     # This part is extracted from scipy.linalg.diagblock
@@ -302,17 +233,19 @@ def merge_covs(covdf):
         c += cc
     diags["BEG"] = beg
     diags["END"] = end
+    # reset multindex to use loc method
+    diags = diags.set_index(['MAT','MT','MAT1','MT1']).sort_index()
     # Process off-diagonal blocks
     for (mat,mt,mat1,mt1),row in covdf.query(query_offdiag).iterrows():
         cov = row.COV
         # interpolate x axis (rows)
-        covk = covdf.loc[mat,mt,0,mt]
+        covk = diags.loc[mat,mt,0,mt]
         Ek = list(covk.COV.index)
         cov = pandas_interpolate(cov, Ek, method='zero', axis='rows')
         # interpolate y axis (cols)
         if mat1 == 0:
             mat1 = mat
-        covl = covdf.loc[mat1,mt1,0,mt1]
+        covl = diags.loc[mat1,mt1,0,mt1]
         El = list(covl.COV.index)
         cov = pandas_interpolate(cov, El, method='zero', axis='cols')
         C[covk.BEG:covk.END,covl.BEG:covl.END] = cov
@@ -338,13 +271,13 @@ tape = pd.DataFrame([[int(x[66:70]), int(x[70:72]), int(x[72:75]), x, int(x[66:7
 tape = tape.set_index(['MAT','MF','MT']).sort_index() # Multi-indexing
 tape['DATA'] = tape['TEXT'].apply(process_section)
 
-C = merge_covs( extract_cov33(tape) )
-extract_xs(tape)
+df_cov = merge_covs( extract_cov33(tape) )
+df_xs = extract_xs(tape)
 
 from sandy.cov import Cov
 NSMP = 100
-df_samples = pd.DataFrame( Cov(C.as_matrix()).sampling(NSMP), index = C.index )
-unique_ids = list(set(zip(C.index.get_level_values(0), C.index.get_level_values(1))))
+df_samples_xs = pd.DataFrame( Cov(df_cov_xs.as_matrix()).sampling(NSMP), index = df_cov_xs.index )
+unique_ids = list(set(zip(df_cov.index.get_level_values(0), df_cov_xs.index.get_level_values(1))))
 #idx = pd.IndexSlice
 #df_samples.loc[idx[:, :, ['C1', 'C3']], idx[:, 'foo']]
 for ismp in range(NSMP):
@@ -353,152 +286,3 @@ for ismp in range(NSMP):
         pass
 
 write_mf33_mt(P)
-
-
-class File:
-    """ General text file """
-
-    @staticmethod
-    def isfile(filename):
-        """
-        Check if file exists.
-
-        Inputs:
-            - filename :
-                (string) path+name of the file
-        """
-        from os.path import isfile
-        if not isfile(filename):
-            logging.error("FILE : File '{}' does not exist".format(filename))
-            sys.exit()
-
-    def __init__(self, filename):
-        """
-        Initialize file.
-
-        Inputs:
-            - filename :
-                (string) path+name of the file
-
-        ..Important::
-            Use encoding="ascii" and errors="surrogateescape" when opening the
-            file in order to process files in ASCII compatible encoding such as
-            `utf-8` and `latin-1`.
-        """
-        self.filename = filename
-        self.f = open(self.filename, encoding="ascii", errors="surrogateescape")
-
-    @property
-    def filename(self):
-        """
-        Absolute file path + name.
-        """
-        return self._filename
-
-    @filename.setter
-    def filename(self, filename):
-        from os.path import expanduser, abspath
-        _f = abspath(expanduser(filename))
-        File.isfile(_f)
-        self._filename = _f
-
-    @property
-    def path(self):
-        """
-        Absolute file path.
-        """
-        from os.path import split
-        return split(self.filename)[0]
-
-    @property
-    def name(self):
-        """
-        File basename.
-        """
-        from os.path import split
-        return split(self.filename)[1]
-
-    def read(self):
-        """
-        Load the content of input object `stream`.
-
-        Outputs:
-            - text :
-                (list of strings) text in the file
-        """
-        try:
-            text = self.f.readlines()
-            return text
-        except:
-            logging.error("FILE : Cannot read file '{}'".format(self.filename))
-            sys.exit()
-
-    def load_yaml(self, msg="ERROR! Cannot yaml.load input file"):
-        """
-        Description
-        ===========
-        Load the content of input object *stream*.
-
-        Outputs
-        ======
-         - *load*: text in yaml format
-        """
-        import yaml
-        try:
-            load = yaml.load(self.f)
-            return load
-        except yaml.YAMLError as exc:
-            sys.exit(msg + " '{}'".format(self.filename))
-
-#import pytest
-#
-#class TestFile:
-#
-#    fileyaml = '../test_objects/input.yaml'
-#    filenotyaml = '../test_objects/input.not_yaml'
-#    filenotexists = 'nonexistingfile'
-#
-#    def test_is_not_file(self):
-#        from files import File
-#        with pytest.raises(SystemExit):
-#            File.isfile(self.filenotexists)
-#
-#    def test_is_file(self):
-#        from files import File
-#        File.isfile(self.fileyaml)
-#
-#    def test_init_file(self):
-#        from files import File
-#        F = File(self.fileyaml)
-#        assert F.name == 'input.yaml'
-#
-#    def test_read_file(self):
-#        from files import File
-#        F = File(self.fileyaml)
-#        assert F.read() == open(self.fileyaml).readlines()
-#
-#    def test_load_yaml_file(self):
-#        from files import File
-#        F = File(self.fileyaml)
-#        load = F.load_yaml()
-#        assert 'replace' in load
-#        assert 'mat' in load['replace']
-#        assert load['replace']['mat'] == 2631
-#        assert 'endf1' in load['replace']
-#        assert load['replace']['endf1'] == 'file1'
-#        assert 'endf2' in load['replace']
-#        assert load['replace']['endf2'] == 'file2'
-#        assert 'mf' in load['replace']
-#        assert load['replace']['mf'] == 3
-#        assert 'mt' in load['replace']
-#        assert load['replace']['mt'] == 102
-#        assert 'emin' in load['replace']
-#        assert load['replace']['emin'] == 1e6
-#        assert 'emax' in load['replace']
-#        assert load['replace']['emax'] == 10e6
-#
-#    def test_load_notyaml_file(self):
-#        from files import File
-#        F = File(self.filenotyaml)
-#        with pytest.raises(SystemExit):
-#            F.load_yaml()
