1cda81e118182e66ecd88ea691fab992252a13ce
Luca Fiorito <lucafiorito.11@gmail.com>
2018-04-25 21:40:29 +0200

    finished mass_check functions for jeff33

--- sandy/endf6/files.py
+++ sandy/endf6/files.py
@@ -52,8 +52,7 @@ def split(file):
     """
     import re
 #    pattern = ".{72}[ 0]{3}.{5}\n?" # this pattern created problems in 28-Ni-58g.jeff33
-
-    pattern = ".{72}  0.{5}\n"
+    pattern = ".{72}  0[ 0-9]{5}\n|.{68}-1 0  0[ 0-9]{5}" # problems in 28-Ni-58g.jeff33 where one line exceeds 80 chars
     text = open(file).read()
     U = re.split(pattern, text)
     return filter(None, U) # remove empty lines
@@ -72,7 +71,7 @@ def split2df(file):
 def split2df_byZAM(file):
     tape = split2df(file)
     byZAM = tape.query("MF==1 & MT==451")
-    byZAM["ZAM"] = byZAM.TEXT.apply(lambda x: int(float(read_float(x[:11]))*10+int(x[100:111])))
+    byZAM["ZAM"] = byZAM.TEXT.apply(lambda x: int(float(read_float(x[:11]))*10+int(x[100:111]))).values
     byZAM = byZAM.drop(["MF", "MT", "TEXT"], axis=1)
     return tape.merge(byZAM, how="left", on="MAT").drop("MAT", axis=1)
 
