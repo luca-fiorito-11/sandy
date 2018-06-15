9a0c3f5a907495880acc95aaf6d7945924184e27
Luca Fiorito <lucafiorito.11@gmail.com>
2018-06-15 15:39:37 +0200

    ongoing major restructuring

---
+++ sandy/formats/MF33/section.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 14 09:23:33 2018
+
+@author: fiorito_l
+"""
+from ..records2 import read_cont, read_control, read_list
+from ..utils import Section
+import pdb
+
+def read(text):
+    str_list = text.splitlines()
+    MAT, MF, MT = read_control(str_list[0])[:3]
+    out = {"MAT" : MAT, "MF" : MF, "MT" : MT}
+    i = 0
+    C, i = read_cont(str_list, i)
+    # Subsections are given as dictionary values.
+    # Keys are MAT1*100+MT1
+    out.update({"ZA" : C.C1, "AWR" : C.C2, "MTL" : C.L2, "SUB" : {}})
+    for j in range(C.N2):
+        C, i = read_cont(str_list, i)
+        sub = {"XMF1" : C.C1, "XLFS1" : C.C2, "MAT1" : C.L1, "MT1" : C.L2}
+        NC = C.N1
+        NI = C.N2
+        NCDICT = {}
+        for k in range(NC):
+            C, i = read_cont(str_list, i)
+            subsub = {"LTY" : C.L2}
+            if subsub["LTY"] == 0:
+                L, i = read_list(str_list, i)
+                subsub.update({"E1" : L.C1, "E2" : L.C2,
+                               "CI" : L.B[:L.N2], "XMTI" : L.B[L.N2:]})
+            elif subsub["LTY"] in (1,2,3):
+                L, i = read_list(str_list, i)
+                subsub.update({"E1" : L.C1, "E2" : L.C2, "MATS" : L.L1, "MTS" : L.L2,
+                               "XMFS" : L.B[0], "XLFSS" : L.B[1],
+                               "EI" : L.B[2:2+L.N2], "WEI" : L.B[2+L.N2:]})
+            NCDICT.update({k : subsub})
+        sub.update({"NC" : NCDICT})
+        NIDICT = {}
+        for k in range(NI):
+            L, i = read_list(str_list, i)
+            subsub = {"LB" : L.L2}
+            if subsub["LB"] in range(5):
+                subsub.update({"LT" : L.L1, "NT" : L.NPL, "NP" : L.N2})
+                if subsub["LT"] == 0:
+                    subsub.update({"EK" : L.B[::2], "FK" : L.B[1::2]})
+                else:
+                    pdb.set_trace()
+                    Nk = subsub["NP"] - subsub["LT"]
+                    ARRk = L.B[:Nk]
+                    ARRl = L.B[Nk:]
+                    subsub.update({"EK" : ARRk[:Nk/2], "FK" : ARRk[Nk/2:],
+                                   "EL" : ARRl[:subsub["LT"]], "FL" : ARRl[subsub["LT"]:]})
+            elif subsub["LB"] == 5:
+                subsub.update({"LS" : L.L1, "NT" : L.NPL, "NE" : L.N2,
+                               "EK" : L.B[:L.N2], "FKK" : L.B[L.N2:]})
+            elif subsub["LB"] == 6:
+                subsub.update({"NT" : L.NPL, "NER" : L.N2, "NEC" : (L.NPL-1)//L.N2})
+                subsub.update({"EK" : L.B[:subsub["NER"]]})
+                subsub.update({"EL" : L.B[subsub["NER"]:subsub["NER"]+subsub["NEC"]]})
+                subsub.update({"FKL" : L.B[subsub["NER"]+subsub["NEC"]:]})
+            elif subsub["LB"] in (8,9):
+                subsub.update({"LT" : L.L1, "NT" : L.NPL, "NP" : L.N2})
+                subsub.update({"EK" : L.B[:subsub["NP"]], "FK" : L.B[subsub["NP"]:]})
+            else:
+                pdb.set_trace()
+            NIDICT.update({k : subsub})
+        sub.update({"NI" : NIDICT})
+        out["SUB"].update({sub["MAT1"]*1000+sub["MT1"] : sub})
+    return Section(out)
