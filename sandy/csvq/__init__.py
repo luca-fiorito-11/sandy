02ac38fa956d37c9ee0a10f06cabe3a00513245c
Luca Fiorito <lucafiorito.11@gmail.com>
2018-07-12 11:49:09 +0200

    minor updates

--- sandy/csvq/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-import pandas as pd
-
-elements = pd.read_csv(os.path.join(__path__[0], "elements.csv")).set_index("Z")
-metastates = pd.read_csv(os.path.join(__path__[0], "metastates.csv")).set_index("LISO")
