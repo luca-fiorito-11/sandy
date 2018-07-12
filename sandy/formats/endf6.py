eaf4d83ff9e6a935a53ef1662986438be1cb4387
Luca Fiorito <lucafiorito.11@gmail.com>
2018-07-12 11:42:58 +0200

    some restructuring of tests

--- sandy/formats/endf6.py
+++ sandy/formats/endf6.py
@@ -554,28 +554,6 @@ def cov_interp(df, interp_column, method='zero', axis='both'):
 
 
 
-#def extract_mu(tape):
-##    NLMAX = 0
-#    keys = []
-#    ListPc = []
-#    for chunk in tape.query('MF==4').DATA:
-#        if "LPC" in chunk:
-#            # check interpolation
-#            for e,sub in sorted(chunk["LPC"]["E"].items()):
-#                keys.append((chunk["MAT"], chunk["MT"], e))
-##                NPL = len(sub["P"])
-##                P[i,:NPL] = sub["P"]
-##                NLMAX = max(NLMAX, NPL)
-#                ListPc.append(sub["P"])
-#    index = pd.MultiIndex.from_tuples(keys, names=("MAT", "MT", "E"))
-#    DfPc = pd.DataFrame(ListPc, index=index).fillna(0)
-#    DfPc.columns = range(1, DfPc.shape[1]+1)
-#    DfPc.columns.name = "LegCoeff"
-#    # print warning if exceeded NLMAX
-#    return DfPc
-
-
-
 #def write_decay_data_csv(tape, filename):
 #    df = tape.query("MF==8 & MT==457")
 #    df['Z'] = df.DATA.apply(lambda x : int(x['ZA']//1000))
@@ -590,296 +568,43 @@ def cov_interp(df, interp_column, method='zero', axis='both'):
 #    df['EHP'] = df.DATA.apply(lambda x : x['E'][2])
 #    df['DEHP'] = df.DATA.apply(lambda x : x['DE'][2])
 #    df[['Z','A','M','HL','DHL','ELP','DELP','EEM','DEEM','EHP','DEHP']].to_csv(filename, index=False)
-
-
-@pytest.fixture(scope="module")
-def testPu9():
-    from sandy.data_test import Pu9
-    tape = Endf6.from_text("\n".join(Pu9.endf6))
-    assert (tape.index.get_level_values("MAT").unique() == 9437).all()
-    return tape
-
-@pytest.fixture(scope="module")
-def testH1():
-    from sandy.data_test import H1
-    tape = Endf6.from_text("\n".join(H1.pendf))
-    assert (tape.index.get_level_values("MAT").unique() == 125).all()
-    return tape
-
-@pytest.fixture(scope="module")
-def testFe56():
-    from sandy.data_test import Fe56
-    tape = Endf6.from_text("\n".join(Fe56.endf6))
-    assert (tape.index.get_level_values("MAT").unique() == 2631).all()
-    return tape
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.info
-def test_read_info(testPu9):
-    S = testPu9.read_section(9437, 1, 451)
-    from .MF1 import write
-    text = write(S)
-    assert testPu9.TEXT.loc[9437,1,451] == text
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.info
-def test_update_info(testPu9):
-    testPu9.loc[9437,3,1].TEXT = "\n".join(testPu9.loc[9437,3,1].TEXT.splitlines()[:10]) + "\n"
-    testPu9 = Endf6(testPu9.drop([(9437,3,102)]))
-    new = testPu9.update_info()
-    recordsold = testPu9.read_section(9437,1,451)["RECORDS"]
-    recordsnew = new.read_section(9437,1,451)["RECORDS"]
-    assert (3,102,147,1) in recordsold
-    assert (3,102,147,1) not in recordsnew
-    assert (3,1,188,1) in recordsold
-    assert (3,1,10,1) in recordsnew
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-def test_read_xs(testPu9):
-    S = testPu9.read_section(9437, 3, 102)
-    from .MF3 import write
-    text = write(S)
-    assert testPu9.TEXT.loc[9437,3,102] == text
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-def test_extract_xs(testH1):
-    testH1.get_xs(listmat=[125], listmt=[1,2,102,4])
-    xs = testH1.get_xs(listmat=[9437])
-    assert xs.empty
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-def test_reconstruct_xs(testH1):
-    xs = testH1.get_xs()
-    xs[(125,51)] = 1
-    xs[(125,2)] = xs[(125,102)] = xs[(125,1)]
-    SUM = xs[(125,1)].values*2 + 1
-    rec_xs = xs.reconstruct_sums(drop=False)
-    assert (rec_xs[(125,4)].values == 1).all()
-    assert np.isclose(rec_xs[(125,1)].values,SUM).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-def test_update_xs(testH1):
-    xs = testH1.get_xs()
-    xs[(125,2)] = 1
-    new = testH1.update_xs(xs)
-    assert (new.read_section(125,3,2)["XS"] == 1).all()
-    assert (testH1.read_section(125,3,2)["XS"] != new.read_section(125,3,2)["XS"]).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-@pytest.mark.cov
-def test_read_xs_cov(testPu9):
-    testPu9.read_section(9437, 33, 1)
-    testPu9.read_section(9437, 33, 2)
-    testPu9.read_section(9437, 33, 18)
-    testPu9.read_section(9437, 31, 456)
-    testPu9.read_section(9437, 33, 102)
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.xs
-@pytest.mark.cov
-def test_extract_xs_cov(testPu9):
-    testPu9.get_xs_cov()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.nubar
-def test_read_nubar(testPu9):
-    from .MF1 import write
-    for mt in (452,455,456):
-        S = testPu9.read_section(9437, 1, mt)
-        text = write(S)
-        assert testPu9.TEXT.loc[9437,1,mt] == text
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.nubar
-def test_extract_nubar(testPu9):
-    testPu9.get_nubar(listmt=[452,456])
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.nubar
-def test_reconstruct_nubar(testPu9):
-    nubar = testPu9.get_nubar()
-    nubar[(9437,455)] = 1
-    SUM = nubar[(9437,456)].values + 1
-    rec_nubar = nubar.reconstruct_sums()
-    assert np.isclose(rec_nubar[(9437,452)].values,SUM).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.nubar
-def test_update_nubar(testPu9):
-    nubar = testPu9.get_nubar()
-    nubar[(9437,452)] = 1
-    new = testPu9.update_nubar(nubar)
-    assert (new.read_section(9437,1,452)["NUBAR"] == 1).all()
-    assert (testPu9.read_section(9437,1,452)["NUBAR"] != new.read_section(9437,1,452)["NUBAR"]).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-def test_read_lpc(testFe56):
-    from .MF4 import write
-    S = testFe56.read_section(2631, 4, 2)
-    assert S["LTT"] == 3
-    assert "LPC" in S
-    assert S["LPC"]["NE"] == len(S["LPC"]["E"]) == 1782
-    assert "TAB" in S
-    assert S["TAB"]["NE"] == len(S["TAB"]["E"]) == 28
-    text = write(S)
-    assert testFe56.TEXT.loc[2631,4,2] == text
-    for mt in range(51,83):
-        S = testFe56.read_section(2631, 4, mt)
-        text = write(S)
-        assert testFe56.TEXT.loc[2631,4,mt] == text
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-def test_extract_lpc(testFe56):
-    testFe56.get_lpc()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-def test_convert_lpc(testFe56):
-    Lpc = testFe56.get_lpc()
-    C = Lpc.to_tab(2631, 2, 1e-5)
-    assert (C == 0.5).all()
-    C = Lpc.to_tab(2631, 2, 1e4)
-    assert (C >= 0).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-def test_addpoints_lpc(testFe56):
-    lpcold = testFe56.get_lpc(listmt=[2])
-    lpcnew = lpcold.add_points([1e4, 1e5, 1e6])
-    assert 1e4 in lpcnew.loc[2631,2].index
-    assert 1e4 not in lpcold.loc[2631,2].index
-    assert 1e5 in lpcnew.loc[2631,2].index
-    assert 1e5 not in lpcold.loc[2631,2].index
-    assert 1e6 in lpcnew.loc[2631,2].index
-    assert 1e6 in lpcold.loc[2631,2].index
-    lpcnew = lpcold.add_points([])
-    assert (lpcnew.index == lpcold.index).all()
-    lpcold = testFe56.get_lpc(listmt=[810])
-    lpcnew = lpcold.add_points([1e4])
-    assert 1e4 not in lpcnew.loc[2631,810].index
-    assert 1e4 not in lpcold.loc[2631,810].index
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-def test_update_lpc(testFe56):
-    lpc = testFe56.get_lpc()
-    new = testFe56.update_lpc(lpc)
-    assert new.TEXT[2631,4,2] == testFe56.TEXT[2631,4,2]
-    lpc.loc[2631,2,1e5:2e7] = 0
-    new = testFe56.update_lpc(lpc)
-    assert new.TEXT[2631,4,2] != testFe56.TEXT[2631,4,2]
-    new_sec = new.read_section(2631,4,2)
-    assert (np.array(new_sec["LPC"]["E"][2e7]["COEFF"]) == 0).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-@pytest.mark.cov
-def test_read_lpc_cov(testFe56):
-    S = testFe56.read_section(2631, 34, 2)
-    assert S["NMT1"] == len(S["REAC"]) == 1
-    assert len(S["REAC"][0,2]["P"]) == S["REAC"][0,2]["NL"]*(S["REAC"][0,2]["NL"]+1)//2
-    assert len(S["REAC"][0,2]["P"][1,1]["NI"]) == 3
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.lpc
-@pytest.mark.cov
-def test_extract_lpc_cov(testFe56):
-    C = testFe56.get_lpc_cov()
-    assert C.index.names == C.columns.names == ['MAT', 'MT', 'L', 'E']
-    assert (C.values == C.values.T).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-def test_read_chi(testPu9):
-    S = testPu9.read_section(9437, 5, 18)
-    from .MF5 import write
-    text = write(S)
-    assert testPu9.TEXT.loc[9437,5,18] == text
-
-@pytest.fixture(scope="module")
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-def testPu9chi(testPu9):
-    return testPu9.get_edistr()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-def test_extract_chi(testPu9chi):
-    testPu9chi.add_points([1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5])
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-def test_normalize_chi(testPu9chi):
-    chi = testPu9chi.normalize()
-    for i,v in chi.iterrows():
-        dx = v.index.values[1:] - v.index.values[:-1]
-        y = (v.values[1:]+v.values[:-1])/2
-        assert np.isclose(y.dot(dx), 1, rtol=1e-10)
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-def test_update_chi(testPu9, testPu9chi):
-    testPu9chi.loc[(9437,18,1,1e-5)] = 1
-    new = testPu9.update_edistr(testPu9chi)
-    new_sec = new.read_section(9437,5,18)
-    assert (new_sec["PDISTR"][1]["EIN"][1e-5]["EDISTR"] == 1).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-@pytest.mark.cov
-def test_read_chi_cov(testPu9):
-    S = testPu9.read_section(9437, 35, 18)
-    assert len(S["SUB"]) == S["NK"] == 8
-    for k,v in S["SUB"].items():
-        assert v["LS"] == 1
-        assert v["LB"] == 7
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.chi
-@pytest.mark.cov
-def test_extract_chi_cov(testPu9):
-    C = testPu9.get_edistr_cov()
-    assert C.index.names == ['MAT', 'MT', 'ELO', 'EHI', 'EOUT']
-    assert C.columns.names == ['MAT', 'MT', 'ELO', 'EHI', 'EOUT']
-    assert (C.values == C.values.T).all()
-
-@pytest.mark.formats
-@pytest.mark.endf6
-@pytest.mark.write
-def test_write_to_string(testH1):
-    string = testH1.write_string()
-    newtape = Endf6.from_text(string)
-    assert testH1.equals(newtape)
+    
+#def get_masses(file):
+#    print(file)
+#    tape = e6.split2df_byZAM(file)
+#    tape["AWR"] = tape["TEXT"].apply(lambda x: float(read_float(x[11:22])))
+#    return tape.drop("TEXT", axis=1)
+#
+#def check_masses(files):
+#    return [ get_masses(file) for file in files ]
+#
+#def check_masses_jeff33(path):
+#    """
+#    Given a path with ENDF-6 files, compare the masses in each section of each file
+#    with those given in the RDD JEFF3.3 file.
+#    Produce a csv summary output.
+#    """
+#    from sandy.data_test import __file__ as td
+#    RDD = join(dirname(realpath(td)), r"RDD.jeff33")
+#    DfRDD = check_masses([RDD])[0].query("MF==1 & MT==451")#.rename(columns={"AWR": "AWR_RDD"})
+#    DfN = pd.concat(check_masses(map(lambda x : join(path, x), listdir(path))))
+#    C = DfN.merge(DfRDD.drop(["MF","MT"], axis=1), how="left", on=["ZAM"], suffixes=("_N","_RDD"))
+#    C["RATIO"] = (C.AWR_N/C.AWR_RDD).values
+#    C["MATCH"] = np.isclose(C["RATIO"], 1, rtol=1e-6)
+#    grouped = C[["ZAM","AWR_N"]].set_index("ZAM").groupby("ZAM")
+#    df = grouped.agg(['count', 'min', 'max', lambda x:x.value_counts().index[0]])
+#    df.columns = df.columns.droplevel(0)
+#    df = df.merge(DfRDD[['ZAM','AWR']], how='left', left_index=True, right_on=['ZAM'])
+#    df["Z"] = np.floor(df.ZAM/10000)
+#    df["A"] = np.floor((df.ZAM - df.Z*10000)/10)
+#    df["M"] = np.floor(df.ZAM - df.Z*10000 - df.A*10)
+#    df.rename(columns={"<lambda>" : "most freq", "AWR" : "ref"}, inplace=True)
+#    df.set_index(['Z','A','M'], inplace=True)
+#    df.drop("ZAM", axis=1, inplace=True)
+#    df["message"] = (np.isclose(df["min"], df["ref"], rtol=1e-6) & (np.isclose(df["min"], df["ref"], rtol=1e-6)))
+#    df["message"] = df.message.replace({True: "OK", False: "WARNING"})
+#    df.to_csv("jeff33_masses.csv")
+#
+#
+##A=get_masses(r"X:\aaa\78-Pt-192g.jeff33")
+#check_masses_jeff33(r"X:\aaa")
--- sandy/interaction/__init__.py
+++
@@ -1,1 +0,0 @@
-from .ndec_xml import *
