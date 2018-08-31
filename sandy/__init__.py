e3e28c4b3739dc3ce9f69c1473a500f3d5620994
Luca Fiorito <lucafiorito.11@gmail.com>
2018-08-31 17:12:38 +0200

    added call to class DuplicateFilter

--- sandy/__init__.py
+++ sandy/__init__.py
@@ -4,11 +4,26 @@ import sys
 from sandy.formats import *
 
 class ShutdownHandler(logging.Handler):
+    """Trigger exit on errors.
+    """
     def emit(self, record):
         logging.shutdown()
         sys.exit(1)
+
+class DuplicateFilter(object):
+    """Define a filter which keeps track of what was logged, and attach it to 
+    your logger for the duration of a loop.
+    """
+    def __init__(self):
+        self.msgs = set()
+
+    def filter(self, record):
+        rv = record.msg not in self.msgs
+        self.msgs.add(record.msg)
+        return rv
 
 FORMAT = '%(levelname)s:  %(message)s'
 logging.basicConfig(format=FORMAT)
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger().addHandler(ShutdownHandler(level=40))
+logging.getLogger().addFilter(DuplicateFilter())
