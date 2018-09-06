2543cf988cfbf667a5453ad097ad5f928495a224
Luca Fiorito <lucafiorito.11@gmail.com>
2018-09-06 15:01:29 +0200

    added replace option

---
+++ sandy/formats/format_tests/utils_test.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 10:08:49 2018
+
+@author: Fiorito_L
+"""
+import pytest
+import os
+
+from .. import Endf6
+from ...data import Pu9
+
+__author__ = "Luca Fiorito"
+
+@pytest.fixture(scope="module")
+def Pu9endf():
+    return Endf6.from_text("\n".join(Pu9.endf6))
+
+@pytest.mark.formats
+@pytest.mark.replace
+def test_replace_endf6(Pu9endf):
+    file = os.path.join(Pu9.__path__[0], "pu239.pendf")
+    Pu9pendf = Endf6.from_file(file)
+    df = Pu9endf.add_sections(file, {2: [152], 10: "all"})
+    assert (df.loc[9437,10,42] == Pu9pendf.loc[9437,10,42]).all()
+    assert not (df.loc[9437,10,42] == Pu9endf.loc[9437,10,42]).all()
+    assert (9437,2,152) in df.index
+    assert (9437,2,152) not in Pu9endf.index
+    assert (df.loc[9437,2,152] == Pu9pendf.loc[9437,2,152]).all()
+    assert not (df.loc[9437,3,2] == Pu9pendf.loc[9437,3,2]).all()
+    assert (df.loc[9437,3,2] == Pu9endf.loc[9437,3,2]).all()
--- sandy/sampling/__main__.py
+++ sandy/sampling/__main__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from sandy.sampling import sampling
+from sandy.sampling.sampling import run
 
 if __name__ == "__main__":
-    sampling.run()
+    run()
 
