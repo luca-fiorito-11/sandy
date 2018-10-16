1b4be785be46c57bc2b0a58ee4e3b5962bd1af80
Luca Fiorito <lucafiorito.11@gmail.com>
2018-10-16 17:37:46 +0200

    built API for fission yields

--- sandy/sampling/utils.py
+++ sandy/sampling/utils.py
@@ -8,19 +8,25 @@ Created on Mon Sep 24 09:29:32 2018
 import pandas as pd
 
 __author__ = "Luca Fiorito"
-__all__ = ["LpcSamples", "EdistrSamples"]
+__all__ = ["LpcSamples", "EdistrSamples", "FySamples"]
 
 class LpcSamples(pd.DataFrame):
     """samples for Legendre Polynomial coefficients.
     
-    index :
-        - MAT number
-        - MT number
-        - order of Legendre polynomial L
-        - incominig neutron energy
+    Index
+    -----
+    MAT : `int`
+        MAT number
+    MT : `int`
+        MT number
+    L : `int`
+        order of Legendre polynomial
+    E : `float`
+        incoming energy
     
-    columns:
-        - sample indices
+    Columns
+    -------
+    sample indices
     """    
 
     def __init__(self, *args, **kwargs):
@@ -35,20 +41,55 @@ class LpcSamples(pd.DataFrame):
 class EdistrSamples(pd.DataFrame):
     """samples for Tabulated energy distributions.
     
-    index :
-        - MAT number
-        - MT number
-        - lower bound for incoming energy ELO
-        - upper bound for incoming energy EHI
-        - outgoing neeutron energy EOUT
+    Index
+    -----
+    MAT : `int`
+        MAT number
+    MT : `int`
+        MT number
+    ELO : `float`
+        lower bound for incoming energy
+    EHI : `float`
+        upper bound for incoming energy
+    EOUT : `float`
+        outgoing neutron energy
     
-    columns:
-        - sample indices
+    Columns
+    -------
+    sample indices
     """    
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.index.names = ["MAT", "MT", "ELO", "EHI", "EOUT"]
+        ncols = len(self.columns)
+        self.columns = range(1, ncols+1)
+        self.columns.name = "SMP"
+
+
+
+class FySamples(pd.DataFrame):
+    """Samples for fission yields.
+    
+    Index
+    -----
+    MAT : `int`
+        MAT number
+    MT : `int`
+        MT number
+    E : `float`
+        incoming neutron energy
+    ZAM : `int`
+        ZZZ * 10000 + A * 10 + M
+    
+    Columns
+    -------
+    sample indices
+    """    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.index.names = ["MAT", "MT", "E", "ZAM"]
         ncols = len(self.columns)
         self.columns = range(1, ncols+1)
         self.columns.name = "SMP"
