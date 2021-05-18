6d9719defe92b5f006e4cb7a8e36d8c8e48ac06c
Luca Fiorito <lfiorito@sckcen.be>
2021-05-18 22:15:31 +0200

    update

--- conftest.py
+++ conftest.py
@@ -10,3 +10,4 @@ pd.options.display.float_format = '{:.5e}'.format
 @pytest.fixture(autouse=True, scope='session')
 def pandas_terminal_width():
     pd.set_option('display.max_columns', 1000)
+    pd.set_option('display.width', 1000)
