da2d97078663f4b857193f7ded379f7db6e9283c
Luca Fiorito <lfiorito@sckcen.be>
2021-05-18 16:20:53 +0200

    update

---
+++ sandy/constants.py
@@ -0,0 +1,11 @@
+__author__ = "Luca Fiorito"
+__all__ = [
+    ]
+
+# conversion coefficient MeV --> MW s
+MeV_MWs = 1.60217733e-19
+
+# molar mass of a neutron in amu
+# source: P. J. Mohr and B. N. Taylor, "The 1998 CODATA Recommended Values
+# of the Fundamental Physics Constants", Version 3.1
+Amn = 1.00866491578
