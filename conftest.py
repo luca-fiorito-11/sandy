4e57866857d35b0bfd1eb95c147297ac167ca417
Luca Fiorito <lfiorito@sckcen.be>
2021-05-18 22:05:17 +0200

    update

--- conftest.py
+++ conftest.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import pytest
+
+
 collect_ignore = ["setup.py"]
+
+pd.options.display.float_format = '{:.5e}'.format
+
+
+@pytest.fixture(autouse=True, scope='session')
+def pandas_terminal_width():
+    pd.set_option('max_cols', 1000)
