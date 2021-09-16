1bb1238e31a26e21b867b06553311b060fbab964
Bengoechea Aitor <Aitor.Bengoechea@sckcen.be>
2021-09-16 10:51:46 +0200

    file mf9 update with examples

--- sandy/sections/mf9.py
+++ sandy/sections/mf9.py
@@ -22,7 +22,7 @@ __all__ = [
 import sandy
 
 
-def read_mf8(tape, mat, mt):
+def read_mf9(tape, mat, mt):
     """
     Parse MAT/MF=9/MT section from `sandy.Endf6` object and return
     structured content in nested dcitionaries.
@@ -40,6 +40,31 @@ def read_mf8(tape, mat, mt):
     -------
     `dict`
         Content of the ENDF-6 tape structured as nested `dict`.
+
+    Examples
+    --------
+    Endf-6 tape structured 'dict' of Radiactive capture of Am-241 from
+    the ENDFB-VII.1 library to obtain the multiplicities for production
+    of radioactive nuclides
+    >>> read_mf9(sandy.get_endf6_file("endfb_71", 'xs', 952410),9543,102)
+    {'MAT': 9543,
+     'MF': 9,
+     'MT': 102,
+     'ZA': 95241.0,
+     'AWR': 238.986,
+     'LIS': 0,
+     'NS': 2,
+     'QM': 5539101.0,
+     'QI': 5539101.0,
+     'IZAP': 95242,
+     'LFS': 0,
+     'NBT': [9],
+     'INT': [3],
+     'E': array([1.000000e-05, 3.690000e-01, 1.000000e+03, 1.000000e+05,
+                 6.000001e+05, 1.000000e+06, 2.000000e+06, 4.000001e+06,
+                 3.000000e+07]),
+     'Y': array([0.9    , 0.9    , 0.8667 , 0.842  , 0.81533, 0.74382, 0.5703 ,
+                 0.52   , 0.52   ])}
     """
     mf = 9
     df = tape._get_section_df(mat, mf, mt)
@@ -82,6 +107,14 @@ def write_mf9(sec):
               the end of each line.
 
     .. important:: The string does not endf with a newline symbol `\n`.
+
+    Examples
+    --------
+    String reproducing the content of a ENDF-6 section for Radiactive capture
+    of Am-241 from the ENDFB-VII.1 library to obtain the multiplicities for
+    production of radioactive nuclides
+    >>> write_mf9(read_mf9(sandy.get_endf6_file("endfb_71", 'xs', 952410),9543,102))
+    ' 95241.0000 238.986000          0          0          2          09543 9102    1\n 5539101.00 5539101.00      95242          0          1          99543 9102    2\n          9          3                                            9543 9102    3\n 1.000000-5 9.000000-1 3.690000-1 9.000000-1 1000.00000 8.667000-19543 9102    4\n 100000.000 8.420000-1 600000.100 8.153300-1 1000000.00 7.438200-19543 9102    5\n 2000000.00 5.703000-1 4000001.00 5.200000-1 30000000.0 5.200000-19543 9102    6'
     """
     lines = sandy.write_cont(
             sec["ZA"],
