f078dfff474ce2367f133f488433596104d54755
Aitor Bengoetxea <aitorabf@gmail.com>
2021-12-17 10:05:44 +0100

    update

--- sandy/sparse_cov.py
+++ sandy/sparse_cov.py
@@ -7,34 +7,34 @@ import pandas as pd
 import numpy as np
 import scipy
 import scipy.sparse as sps
+import scipy.sparse.linalg as spsl
 import sandy
 
 __author__ = "Aitor Bengoechea"
 
 
-class cov:
+class sparse_cov:
+    def __repr__(self):
+        return self.data.__repr__()
+
     def __init__(self, datos):
         self.data = datos
 
     @property
     def data(self):
         """
-        Introduce a DataFrame and transfor into scipy sparse.
+        Covariance matrix as a sparse matrix
 
         Returns
         -------
-        `dict`
-            hierarchical RDD content
+        `array(<NxN sparse matrix of type '<class 'numpy.float64'>`
+            Covariance matrix as a sparse matrix
+
         """
         return self._data
 
     @data.setter
     def data(self, data):
-        """[summary]
-
-        Args:
-            data (pd.DataFrame): [description]
-        """
         data_ = np.array(data)
         self._data = sps.csr_matrix(data_)
 
@@ -43,46 +43,200 @@ class cov:
         return self.data.shape[0]
 
     def eig(self, sort=True):
+        """
+        Extract eigenvalues and eigenvectors.
+
+        Parameters
+        ----------
+        sort : `bool`, optional, default is `True`
+            flag to return sorted eigenvalues and eigenfunctions
+
+        Returns
+        -------
+        `np.array`
+            real part of eigenvalues sorted in descending order
+        `np.array`
+            matrix of eigenvectors
+
+        Examples
+        --------
+        Extract eigenvalues of covariance matrix.
+        >>> eigenvalues = sparse_cov([[1, 0.4],[0.4, 1]]).eig()[0]
+        >>> comparison = eigenvalues == np.array([1.4, 0.6])
+        >>> assert comparison.all() == True
+
+        Extract eigenfunctions of covariance matrix.
+        >>> eigenfunctions = sparse_cov([[1, 0.4],[0.4, 1]]).eig()[1].round(2)
+        >>> comparison = eigenfunctions == np.array([[ 0.71, -0.71], [ 0.71,  0.71]])
+        >>> assert comparison.all() == True
+        """
         return get_eig(self.data, sort=sort)
 
     @property
     def std(self):
+        """
+        Extract standard deviations.
+
+        Returns
+        -------
+        `np.array`
+            1d array of standard deviations
+
+        Examples
+        --------
+        >>> comparison = sparse_cov([[1, 0.4],[0.4, 1]]).std.data == np.array([1., 1.])
+        >>> assert comparison.all() == True
+        """
         cova = self.data
         std = np.sqrt(cova.diagonal())
         return sps.csr_matrix((std))
 
     @property
     def corr(self):
+        """
+        Extract correlation matrix.
+
+        Returns
+        -------
+        `np.array`
+            correlation matrix
+
+        Examples
+        --------
+        >>> correlation = sparse_cov([[4, 2.4],[2.4, 9]]).corr.round(1)
+        >>> comparison = correlation == np.array([[1.0, 0.4], [0.4, 1.0]])
+        >>> assert comparison.all() == True
+        """
         cov = self.data
         coeff = self.std
         corr = cov/coeff.multiply(coeff.T)
         return corr
 
     def invert(self):
-        cov = self.data
-        cov_inv = sps.linalg.inv(cov).toarray()
+        """
+        Method for calculating the inverse matrix.
+
+        Returns
+        -------
+        `sparse_cov`
+            The inverse matrix.
+
+        Examples
+        --------
+        >>> S = sparse_cov(np.diag(np.array([1, 2, 3])))
+        >>> print(np.round(S.invert().data,2))
+        (0, 0)	1.0
+        (1, 1)	0.5
+        (2, 2)	0.33
+        """
+        cov = sps.coo_matrix(self.data).tocsc()
+        lu = spsl.splu(cov)
+        eye = np.eye(cov.shape[0])
+        cov_inv = lu.solve(eye)
         return self.__class__(cov_inv)
 
     def decompose(self):
-        E, V = self.eig(sort=False)
-        E[E < 0] = 0
-        M = sps.csr_matrix(V).dot(sps.diags(np.sqrt(E))).toarray()
-        Q, R = scipy.linalg.qr(M.T)
-        return R.T
+        """
+        Extract lower triangular matrix `L` for which `L*L^T == COV`.
+
+        Returns
+        -------
+        `numpy.ndarray`
+            lower triangular matrix
+
+        Example
+        -------
+        >>> triangular = sparse_cov([[1, 0.4],[0.4, 1]]).decompose()
+        >>> comparison = triangular == np.array([[1. , 0. ], [0.4, 1. ]])
+        >>> assert comparison.all() == True
+        """
+        triangular = sps.tril(self.data, format='csc')
+        return triangular.toarray()
 
     @classmethod
     def from_var(cls, var):
+        """
+        Construct the covariance matrix from the variance vector.
+
+        Parameters
+        ----------
+        var : 1D iterable
+            Variance vector.
+
+        Returns
+        -------
+        `sparse_cov`
+            Object containing the covariance matrix.
+
+        Example
+        -------
+        >>> S = np.array([[1, 2], [3, 4]])
+        >>> sensitivity = sparse_cov.from_var([1, 1])
+        >>> print(sensitivity._gls_Vy_calc(S))
+         (0, 1)	11.0
+         (0, 0)	5.0
+         (1, 1)	25.0
+         (1, 0)	11.0
+        """
         var_ = pd.Series(var).values
         cov = sps.diags(var_)
         return cls(cov.toarray())
 
     @classmethod
     def from_stdev(cls, std):
+        """
+        Construct the covariance matrix from the standard deviation vector.
+
+        Parameters
+        ----------
+        std : `pandas.Series`
+            Standard deviations vector.
+
+        Returns
+        -------
+        `sparse_cov`
+            Object containing the covariance matrix.
+
+        Example
+        -------
+        >>> S = pd.Series(np.array([0, 2, 3]), index=pd.Index([1, 2, 3]))
+        >>> print(sparse_cov.from_stdev(S).data)
+         (1, 1)	4.0
+         (2, 2)	9.0
+        """
         std_ = pd.Series(std).values
         var = std_ * std_
-        return cov.from_var(var)
+        return sparse_cov.from_var(var)
 
     def _gls_Vy_calc(self, S):
+        """
+        2D calculated output using
+        .. math::
+            $$
+            S\cdot V_{x_{prior}}\cdot S.T
+            $$
+
+        Parameters
+        ----------
+        S : 2D iterable
+            Sensitivity matrix (MXN).
+
+        Returns
+        -------
+        `pd.DataFrame`
+            Covariance matrix `Vy_calc` calculated using
+            S.dot(Vx_prior).dot(S.T)
+
+        Example
+        -------
+        >>> S = np.array([[1, 2], [3, 4]])
+        >>> sensitivity = sparse_cov.from_var([1, 1])
+        >>> print(sensitivity._gls_Vy_calc(S))
+         (0, 1)	11.0
+         (0, 0)	5.0
+         (1, 1)	25.0
+         (1, 0)	11.0
+        """
         S_ = pd.DataFrame(S).values
         S_ = sps.csr_matrix(S_)
         Vx_prior = self.data
@@ -90,18 +244,112 @@ class cov:
         return Vy_calc
 
     def _gls_G(self, S, Vy_extra):
+        """
+        2D calculated output using
+        .. math::
+            $$
+            S\cdot V_{x_{prior}}\cdot S.T + V_{y_{extra}}
+            $$
+
+        Parameters
+        ----------
+        S : 2D iterable
+            Sensitivity matrix (MXN).
+        Vy_extra : 2D iterable
+            2D covariance matrix for y_extra (MXM).
+
+        Returns
+        -------
+        `<NxN sparse matrix of type '<class 'numpy.float64'>'`
+            Covariance matrix `G` calculated using
+            S.dot(Vx_prior).dot(S.T) + Vy_extra
+
+        Example
+        -------
+        >>> S = np.array([[1, 2], [3, 4]])
+        >>> sensitivity = sparse_cov.from_var([1, 1])
+        >>> Vy = np.diag(pd.Series([1, 1]))
+        >>> print(sensitivity._gls_G(S, Vy))
+         (0, 0)	6.0
+         (0, 1)	11.0
+         (1, 0)	11.0
+         (1, 1)	26.0
+        """
         Vy_extra_ = pd.DataFrame(Vy_extra).values
-        Vy_extra_ = cov(Vy_extra_).data
+        Vy_extra_ = sparse_cov(Vy_extra_).data
         # GLS_sensitivity:
         Vy_calc = self._gls_Vy_calc(S)
         G = Vy_calc + Vy_extra_
         return G
 
     def _gls_G_inv(self, S, Vy_extra):
+        """
+        2D calculated output using
+        .. math::
+            $$
+            \left(S\cdot V_{x_{prior}}\cdot S.T + V_{y_{extra}}\right)^{-1}
+            $$
+
+        Parameters
+        ----------
+        S : 2D iterable
+            Sensitivity matrix (MXN).
+        Vy_extra : 2D iterable
+            2D covariance matrix for y_extra (MXM).
+
+        Returns
+        -------
+        `sparse_cov`
+            Covariance matrix `G_inv` calculated using
+            (S.dot(Vx_prior).dot(S.T) + Vy_extra)^-1
+
+        Example
+        -------
+        >>> S = np.array([[1, 2], [3, 4]])
+        >>> sensitivity = sparse_cov.from_var([1, 1])
+        >>> Vy = np.diag(pd.Series([1, 1]))
+        >>> print(np.round(sensitivity._gls_G_inv(S, Vy).data, 2))
+          (0, 0)	0.74
+          (0, 1)	-0.31
+          (1, 0)	-0.31
+          (1, 1)	0.17
+        """
         G = self._gls_G(S, Vy_extra).toarray()
-        return cov(G).invert()
+        return sparse_cov(G).invert()
 
     def _gls_general_sensitivity(self, S, Vy_extra, threshold=None):
+        """
+        Method to obtain general sensitivity according to GLS
+        .. math::
+            $$
+            V_{x_{prior}}\cdot S.T \cdot \left(S\cdot V_{x_{prior}}\cdot S.T + V_{y_{extra}}\right)^{-1}
+            $$
+
+        Parameters
+        ----------
+        S : 2D iterable
+            Sensitivity matrix (MXN).
+        Vy_extra : 2D iterable
+            2D covariance matrix for y_extra (MXM).
+        threshold : `int`, optional
+            threshold to avoid numerical fluctuations. The default is None.
+
+        Returns
+        -------
+        `GLS`
+            GLS sensitivity for a given Vy_extra and S.
+
+        Example
+        -------
+        >>> S = np.array([[1, 2], [3, 4]])
+        >>> sensitivity = sparse_cov.from_var([1, 1])
+        >>> Vy = np.diag(pd.Series([1, 1]))
+        >>> print(np.round(sensitivity._gls_general_sensitivity(S, Vy), 2))
+          (0, 0)	-0.2
+          (0, 1)	0.2
+          (1, 0)	0.23
+          (1, 1)	0.06
+        """
         S_ = sps.csr_matrix(pd.DataFrame(S).values)
         Vx_prior = self.data
         # GLS_sensitivity:
