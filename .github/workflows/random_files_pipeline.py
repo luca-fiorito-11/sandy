d38b589daf77ba218ffd691df8faa520ef807616
Fiorito Luca <luca.fiorito@sckcen.be>
2022-03-04 15:00:03 +0100

    updated pipeline

--- .github/workflows/random_files_pipeline.py
+++ .github/workflows/random_files_pipeline.py
@@ -9,8 +9,9 @@ samples = int(sys.argv[5])
 
 tape = sandy.get_endf6_file(library, 'xs', nuclide)
 name = "file.endf6"
+folder = "random_files"
 tape.to_file(name)
-cline = f"{name} --samples {samples} --temperatures {temperature} --mf 33"
+cline = f"{name} --samples {samples} --temperatures {temperature} --mf 33 -D {folder}"
 if fmt == "ACE":
     cline += " --acer"
 out = sandy.sampling(cline.split())
@@ -20,9 +21,12 @@ basename = pendf.split(".")[0]
 os.remove(pendf)
 os.remove(name)
 if fmt == "ACE":
-    for f in glob.glob(f"{name}*"):
+    for f in glob.glob(f"**/{name}*", recursive=True):
         os.remove(f)
 else:
-    for f in glob.glob(f"{name}*"):
+    for f in glob.glob(f"**/{name}*", recursive=True):
         base, smp = f.split("-")
-        shutil.move(f, "-".join([basename, smp]))
+        path, base = os.path.split(base)
+        new_name = "-".join([basename, smp])
+        new_file = os.path.join(path, new_name)
+        shutil.move(f, new_file)
