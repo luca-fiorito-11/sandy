45310f6ed3b92418b815b7d932c7a7a9176203c4
Luca Fiorito <lucafiorito.11@gmail.com>
2023-02-02 15:17:48 +0100

    read MCNP table 126 (#250)
    Co-authored-by: Luca Fiorito <lfiorito@sckcen.be>

--- sandy/mcnp/output_file.py
+++ sandy/mcnp/output_file.py
@@ -8,6 +8,7 @@ __author__ = "Luca Fiorito"
 
 __all__ = [
         "get_keff",
+        "get_table126",
         "get_table140",
         ]
 
@@ -22,14 +23,45 @@ def get_keff(file):
     return {"KEFF": keff, "ERR": std}
 
 
+def get_table126(file):
+    print(f"reading file '{file}'...")
+    with open(file, 'r') as f:
+        text = f.read()
+
+    PATTERN = "(?:^1neutron.*table 126\n)(?P<table>(?:.*\n)+?)(?P<end>^\s{11}total\s{2})"
+    match = re.search(PATTERN, text, re.MULTILINE).group("table")
+    widths=[9, 10, 12, 14, 13, 14, 13, 13, 13, 13]
+    dtypes = [int] * 5 + [float] * 5
+
+    names = pd.read_fwf(
+        StringIO(match),
+        widths=widths,
+        skip_blank_lines=True,
+        nrows=3,
+        header=None,
+        ).fillna("").apply(lambda col: " ".join(col).strip())
+    names[0] = "cell index"
+
+    df = pd.read_fwf(
+        StringIO(match),
+        widths=widths,
+        skip_blank_lines=True,
+        skiprows=4,
+        names=names,
+    ).ffill()
+    return df.astype(dict(zip(names, dtypes)))
+
+
 def get_table140(file):
     print(f"reading file '{file}'...")
     with open(file, 'r') as f:
         text = f.read()
+
     PATTERN = "(?:^1neutron.*table 140\n)(?P<table>(?:.*\n)+?)(?P<end>^\s+total\s{20})"
     match = re.search(PATTERN, text, re.MULTILINE).group("table")
     widths=[10, 9, 11, 9,] + [12] * 6
     dtypes = [int, int, str, float, int, float, float, float, float, int]
+
     names = pd.read_fwf(
         StringIO(match),
         widths=widths,
@@ -37,6 +69,7 @@ def get_table140(file):
         nrows=2,
         header=None,
         ).fillna("").apply(lambda col: " ".join(col).strip())
+
     df = pd.read_fwf(
         StringIO(match),
         widths=widths,
