6c713004304f12740fe4b50b449f212109d959b1
Barone Luigi <Luigi.Barone@sckcen.be>
2023-03-07 09:43:13 +0100

    Cmmit test

--- notebooks/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
-print(diamonds.cut.isin(["Ideal","Good","Very good"]).head())
-print(diamonds.head())
-print(diamonds[diamonds.cut.isin(["Ideal","Good","Very good"])].head())
-
-
-# 
