ad65f6059e55e4634693b5498d0afd7daefcd44f
Luca Fiorito <lucafiorito.11@gmail.com>
2023-07-07 09:10:27 +0200

    Update make_docs.sh

--- make_docs.sh
+++ make_docs.sh
@@ -7,7 +7,7 @@ sphinx-quickstart --sep --dot _ --language en --suffix .rst --master index --mak
 sed -i '13i\   sandy' api_docs/source/index.rst
 sed -i "s/alabaster/sphinx_rtd_theme/" api_docs/source/conf.py
 # add folder with sources
-sed -i '1 i\import os\nimport sys\nsys.path.insert(0, os.path.abspath(os.path.join("..", "..", "sandy")))\nprint(sys.path)' api_docs/source/conf.py
+sed -i '1 i\import os\nimport sys\nsys.path.insert(0, os.path.abspath(os.path.join("..", "..", "sandy")))\nsys.path.insert(0, os.path.abspath("../"))\nprint(sys.path)' api_docs/source/conf.py
 cat api_docs/source/conf.py
 ls sandy
 pwd
