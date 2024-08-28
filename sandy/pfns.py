5c2de1fd7df7f1ec69558e057137487780f6553d
Federico Grimaldi <federico.grimaldi98@gmail.com>
2024-08-28 13:26:55 +0200

    fist commit

--- sandy/pfns.py
+++ sandy/pfns.py
@@ -527,3 +527,6 @@ class Edistr():
         if not data:
             raise sandy.Error("no tabulated energy distribution was found")
         return cls(data)
+
+    def to_endf6(self):
+        pass
