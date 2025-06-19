d64f9f7a2cc61327ff51384545423bff1f0e6ec8
Luca Fiorito <lucafiorito.11@gmail.com>
2025-06-19 15:08:08 +0200

    bump version

--- sandy/__init__.py
+++ sandy/__init__.py
@@ -53,7 +53,7 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 
-__version__ = '1.1.0-beta1'
+__version__ = '1.1.0-beta3'
 
 
 # Must import submodules after __version__ and everything above.
